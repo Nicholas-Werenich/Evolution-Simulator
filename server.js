e41a222e7fd9923b189fd08e639457849c03b16b
Nicholas <variousdoorknobs@gmail.com>
2025-06-02 00:25:46 -0500

    Fixed catch all route

--- server.js
+++ server.js
@@ -62,10 +62,6 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 //Add creature endpoint
 app.post("/add-creature", async (req, res) => {
 
